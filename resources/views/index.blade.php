7e02f229664c12df0f423da58126a291488a3431
Edsonjorgef1 <edson@equipmoz.org>
2020-08-18 20:33:44 +0200

    Habilitando as Referencias JS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -636,14 +636,14 @@
          </div>
       </div>
       <!-- Bootstrap core JavaScript-->
-      <script src="vendor/jquery/jquery.min.js"></script>
-      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+      <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
       <!-- Core plugin JavaScript-->
-      <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+      <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
       <!-- Owl Carousel -->
-      <script src="vendor/owl-carousel/owl.carousel.js"></script>
+      <script src="{{ asset('assets/vendor/owl-carousel/owl.carousel.js') }}"></script>
       <!-- Custom scripts for all pages-->
-      <script src="js/custom.js"></script>
+      <script src="{{ asset('assets/js/custom.js') }}"></script>
    </body>
 
 </html>
