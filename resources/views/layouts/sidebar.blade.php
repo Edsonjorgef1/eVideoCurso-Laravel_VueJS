00729cb6aebb95755d80b1435afbcc55d6c097f5
Edsonjorgef1 <edson@equipmoz.org>
2020-08-20 18:36:01 +0200

    Refactoração das rotas de sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -13,13 +13,13 @@
                </a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="single-channel.html">
+               <a class="nav-link" href="/single-channel">
                <i class="fas fa-fw fa-user-alt"></i>
                <span>Single Channel</span>
                </a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="video-page.html">
+               <a class="nav-link" href="/video-page">
                <i class="fas fa-fw fa-video"></i>
                <span>Video Page</span>
                </a>
@@ -47,20 +47,20 @@
                </div>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="history-page.html">
+               <a class="nav-link" href="/history-page">
                <i class="fas fa-fw fa-history"></i>
                <span>History Page</span>
                </a>
             </li>
             <li class="nav-item dropdown">
-               <a class="nav-link dropdown-toggle" href="categories.html" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               <a class="nav-link dropdown-toggle" href="/categories" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                <i class="fas fa-fw fa-list-alt"></i>
                <span>Categories</span>
                </a>
                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="categories.html">Movie</a>
-                  <a class="dropdown-item" href="categories.html">Music</a>
-                  <a class="dropdown-item" href="categories.html">Television</a>
+                  <a class="dropdown-item" href="/categories">Movie</a>
+                  <a class="dropdown-item" href="/categories">Music</a>
+                  <a class="dropdown-item" href="/categories">Television</a>
                </div>
             </li>
             <li class="nav-item channel-sidebar-list">
