17f4f2d32bc46950b8e778672d1b8052cd60c196
Edsonjorgef1 <edson@equipmoz.org>
2020-08-22 00:37:22 +0200

    Concluida integracao do Login do template & funcionando

--- resources/views/auth/eVCourses_auth/login.blade.php
+++ resources/views/auth/eVCourses_auth/login.blade.php
@@ -10,23 +10,101 @@
                         <h5 class="mt-3 mb-3">Welcome to Vidoe</h5>
                         <p>It is a long established fact that a reader <br> will be distracted by the readable.</p>
                      </div>
-                     <form action="https://askbootstrap.com/preview/vidoe-v1-1/index.html">
+                     
+                     <!-- LARAVEL FORM -->
+                     <!-- <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form> -->
+
+                    <!-- LARAVEL FORM -->
+                  
+                    <!-- TEMPLATE FORM -->
+                     <form method="POST" action="{{ route('login') }}">
+                        @csrf
                         <div class="form-group">
-                           <label>Mobile number</label>
-                           <input type="text" class="form-control" placeholder="Enter mobile number">
+                           <label>Email</label>
+                           <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                              @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                              @enderror
                         </div>
                         <div class="form-group">
                            <label>Password</label>
-                           <input type="password" class="form-control" placeholder="Password">
+                           <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                              @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                              @enderror
+
+                              <input class="form-check-input" type="hidden" name="remember" id="remember" checked >
                         </div>
                         <div class="mt-4">
                            <div class="row">
                               <div class="col-12">
-                                 <button type="submit" class="btn btn-outline-primary btn-block btn-lg">Sign In</button>
+                                <button type="submit" class="btn btn-outline-primary btn-block btn-lg">Sign In</button>
                               </div>
                            </div>
                         </div>
                      </form>
+
+                   <!-- TEMPLATE FORM -->
                      <div class="text-center mt-5">
                         <p class="light-gray">Don’t have an account? <a href="register.html">Sign Up</a></p>
                      </div>
