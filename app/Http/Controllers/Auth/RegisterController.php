176d28cf20d333e70ab29e832feee2187e094a21
Edsonjorgef1 <edson@equipmoz.org>
2020-08-22 00:50:59 +0200

    1o Passo para integrar o Register Page do template

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,6 +31,10 @@ class RegisterController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+
+    public function showRegistrationForm(){
+        return view ('auth.eVCourses_auth.register');
+    }
     /**
      * Create a new controller instance.
      *
