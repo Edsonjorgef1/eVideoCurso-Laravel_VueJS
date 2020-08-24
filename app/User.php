fbcdd412e1e4163aed3d753bb713f1e24dc7cbd5
Edsonjorgef1 <edson@equipmoz.org>
2020-08-24 17:01:01 +0200

    Queue, Jobs, Notifications, ShouldQueue - Envio de email de confirmação

--- app/User.php
+++ app/User.php
@@ -35,4 +35,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new \App\Notifications\Auth\SendConfirmationEmail($this));
+    }
 }
