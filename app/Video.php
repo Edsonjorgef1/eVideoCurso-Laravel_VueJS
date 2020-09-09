4b5901d335085b9c392c747d0151176c63fa4cac
Edsonjorgef1 <edson@equipmoz.org>
2020-09-09 16:39:20 +0200

    Acessors - Personalizando a duração do vídeo
    Acessors - Personalizando a duração do vídeo

--- app/Video.php
+++ app/Video.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
-    //
+    public function getDurationAttribute($value){
+        $hours = null;
+        $minutes = null;
+        $seconds = null;
+
+        list($hours, $minutes, $seconds) = explode(':', $value);
+
+        return $hours !== '00' ? $value : $minutes . ':' . $seconds;
+    }
 }
